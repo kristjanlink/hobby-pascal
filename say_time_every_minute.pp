4d252e696f235c8b2bf9ce64d76c8b47b5383d47
kristjanlink <kristjanlink007@hotmail.com>
2020-07-23 22:28:39 +0300

    Add files via upload
    New program code added which says the time every minute in Estonian.

---
+++ say_time_every_minute.pp
@@ -0,0 +1,114 @@
+program say_time_every_minute;
+uses crt, mmsystem, sysutils;
+var
+  mins, hours: string;
+
+begin
+  while true do begin
+    writeln('The current time is: ', formatDateTime('hh:nn', time));
+
+    hours := formatDateTime('hh', time);
+
+    if hours < '10' then
+      sndPlaySound('t0.wav', snd_Async or snd_NoDefault);
+    if hours > '19' then begin
+      sndPlaySound('t2.wav', snd_Async or snd_NoDefault);
+      delay(625);
+      sndPlaySound('ty.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+
+    if hours = '00' then
+      sndPlaySound('t0.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '1' then
+      sndPlaySound('t1.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '2' then
+      sndPlaySound('t2.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '3' then
+      sndPlaySound('t3.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '4' then
+      sndPlaySound('t4.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '5' then
+      sndPlaySound('t5.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '6' then
+      sndPlaySound('t6.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '7' then
+      sndPlaySound('t7.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '8' then
+      sndPlaySound('t8.wav', snd_Async or snd_NoDefault);
+    if rightStr(hours, 1) = '9' then
+      sndPlaySound('t9.wav', snd_Async or snd_NoDefault);
+    if hours = '10' then
+      sndPlaySound('t10.wav', snd_Async or snd_NoDefault);
+    if strToInt(hours) in [11..19] then begin
+      delay(625);
+      sndPlaySound('teen.wav', snd_Async or snd_NoDefault);
+    end;
+
+    mins := formatDateTime('nn', time);
+    delay(500); // The delay between saying hours and minutes
+
+    if mins < '10' then begin
+      sndPlaySound('t0.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+    if strToInt(mins) in [20..29] then begin
+      sndPlaySound('t2.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+    if strToInt(mins) in [30..39] then begin
+      sndPlaySound('t3.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+    if strToInt(mins) in [40..49] then begin
+      sndPlaySound('t4.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+    if strToInt(mins) in [50..59] then begin
+      sndPlaySound('t5.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+    if mins > '19' then begin
+      sndPlaySound('ty.wav', snd_Async or snd_NoDefault);
+      delay(625);
+    end;
+
+    if mins = '00' then
+      sndPlaySound('t0.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '1' then
+      sndPlaySound('t1.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '2' then
+      sndPlaySound('t2.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '3' then
+      sndPlaySound('t3.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '4' then
+      sndPlaySound('t4.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '5' then
+      sndPlaySound('t5.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '6' then
+      sndPlaySound('t6.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '7' then
+      sndPlaySound('t7.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '8' then
+      sndPlaySound('t8.wav', snd_Async or snd_NoDefault);
+    if rightStr(mins, 1) = '9' then
+      sndPlaySound('t9.wav', snd_Async or snd_NoDefault);
+    if mins = '10' then
+      sndPlaySound('t10.wav', snd_Async or snd_NoDefault);
+    if strToInt(mins) in [11..19] then begin
+      delay(625);
+      sndPlaySound('teen.wav', snd_Async or snd_NoDefault);
+    end;
+
+    gotoXY(24, 1);
+    repeat
+      write(' ');
+      delay(1000);
+      gotoXY(24, 1);
+      write(':');
+      delay(1000);
+      gotoXY(24, 1);
+    until formatDateTime('nn', time) <> mins;
+    clrscr;
+  end;
+end.
